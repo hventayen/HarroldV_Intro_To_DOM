7d38064e52b86442dcef8c34c853f736795fe905
hventayen <harrold.ventayen@gmail.com>
2025-04-01 18:52:03 -0700

    querySelector

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const test = getElementById('title');
 console.log(test);
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+console.log(one);
+console.log(two);
+console.log(three);
